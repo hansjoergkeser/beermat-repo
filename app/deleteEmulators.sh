1ae0df1b490d53e73d88953e09cd521ab88166f0
Hansjoerg Keser <hansjoerg.kese@idealo.de>
2019-06-04 14:25:56 +0200

    emulator creation and start works now...but tests don't start

--- app/deleteEmulators.sh
+++ app/deleteEmulators.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-${ANDROID_HOME}/tools/bin/avdmanager delete avd -n ScreenshotEmulatorPhone
+#https://gist.github.com/royclarkson/f39115fe0a7ac1f08630
+adb devices | grep "emulator-" | while read -r emulator device; do
+  adb -s ${emulator} emu kill
+done
+avdmanager delete avd -n ScreenshotEmulatorPhone
+
+#~/Library/Android/sdk/tools/bin/avdmanager delete avd -n ScreenshotEmulatorPhone
+#${ANDROID_HOME}/tools/bin/avdmanager delete avd -n ScreenshotEmulatorPhone
