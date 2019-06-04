183cb13bc345add69c73d8c7fa3aa616a2d9884b
Hansjoerg Keser <hansjoerg.kese@idealo.de>
2019-06-04 17:12:39 +0200

    it works, ship it! ... this time for real :-)

--- screenshotRunner.sh
+++ screenshotRunner.sh
@@ -1,24 +1,51 @@
 #!/usr/bin/env bash
-#https://stackoverflow.com/a/44386974/3258117
+# https://stackoverflow.com/a/44386974/3258117
 echo no | avdmanager create avd -f -n ScreenshotEmulatorPhone -k "system-images;android-28;google_apis;x86"
-cat app/nexus5-config.ini > ~/.android/avd/ScreenshotEmulatorPhone.avd/config.ini
 
-#echo no | ~/Library/Android/sdk/tools/bin/avdmanager create avd -f -n ScreenshotEmulatorPhone -k "system-images;android-28;google_apis;x86"
-#echo no | ${ANDROID_HOME}/tools/bin/avdmanager create avd -f -n ScreenshotEmulatorPhone -k "system-images;android-28;google_apis;x86"
+# Overwrite the config.ini of the just created emulator... to get the suitable nexus skin and resolution, etc.
+cat nexus5-config.ini > ~/.android/avd/ScreenshotEmulatorPhone.avd/config.ini
 
-#https://spin.atomicobject.com/2016/03/10/android-test-script/
-WAIT_CMD="adb wait-for-device shell getprop init.svc.bootanim"
+# if avdmanager is not known, you need to set it as environment variable... or you could try this instead:
+# echo no | ~/Library/Android/sdk/tools/bin/avdmanager create avd -f -n ScreenshotEmulatorPhone -k "system-images;android-28;google_apis;x86"
+# echo no | ${ANDROID_HOME}/tools/bin/avdmanager create avd -f -n ScreenshotEmulatorPhone -k "system-images;android-28;google_apis;x86"
+
+# Start the emulator
 emulator -avd ScreenshotEmulatorPhone &
-until $WAIT_CMD | grep -m 1 stopped; do
-  echo "Waiting for emulator..."
-  sleep 1
+adb wait-for-device
+
+# https://spin.atomicobject.com/2016/03/10/android-test-script/
+WAIT_CMD="adb wait-for-device shell getprop init.svc.bootanim"
+until ${WAIT_CMD} | grep -m 1 stopped; do
+    echo "Waiting for emulator..."
+    sleep 1
 done
-printf "\nEmulator is ready now."
 
-./gradlew clean cAT -Pandroid.testInstrumentationRunnerArguments.class=de.hajo.beermat.screenshots.MainActivityScreenshotEspTests -i
+# Emulator still needs time, otherwise the following demo mode commands won't work
+sleep 10
+echo "Emulator is ready now."
 
-#https://gist.github.com/royclarkson/f39115fe0a7ac1f08630
+# Start demo mode
+adb shell settings put global sysui_demo_allowed 1
+# Display time 12:00
+adb shell am broadcast -a com.android.systemui.demo -e command clock -e hhmm 1200
+# Display full mobile data without type
+adb shell am broadcast -a com.android.systemui.demo -e command network -e mobile show -e level 4 -e datatype false
+adb shell am broadcast -a com.android.systemui.demo -e command network -e wifi show -e level 4 -e fully true
+# Hide notifications
+adb shell am broadcast -a com.android.systemui.demo -e command notifications -e visible false
+# Show full battery but not in charging state
+adb shell am broadcast -a com.android.systemui.demo -e command battery -e plugged false -e level 100
+
+# Start tests
+./gradlew clean cAT -Pandroid.testInstrumentationRunnerArguments.class=de.hajo.beermat.screenshots.MainActivityScreenshotEspTests
+
+# Disable demo mode
+adb shell am broadcast -a com.android.systemui.demo -e command exit
+
+# https://gist.github.com/royclarkson/f39115fe0a7ac1f08630
 adb devices | grep "emulator-" | while read -r emulator device; do
-  adb -s ${emulator} emu kill
+    adb -s ${emulator} emu kill
 done
+
+# Remove the emulator
 avdmanager delete avd -n ScreenshotEmulatorPhone
