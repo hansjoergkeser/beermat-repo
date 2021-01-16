f58636e4bf810f8bdfd0d584286103d2a88c5401
Jose-Luis-Nunez <josenunez@outlook.de>
2021-01-16 18:05:50 +0100

    add new screen for beer list

--- app/src/main/java/de/hajo/beermat/LoginActivity.kt
+++ app/src/main/java/de/hajo/beermat/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.annotation.SuppressLint
 import android.annotation.TargetApi
 import android.app.LoaderManager.LoaderCallbacks
 import android.content.CursorLoader
+import android.content.Intent
 import android.content.Loader
 import android.content.pm.PackageManager
 import android.database.Cursor
@@ -15,7 +16,9 @@ import android.os.AsyncTask
 import android.os.Build
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.text.Editable
 import android.text.TextUtils
+import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.ArrayAdapter
@@ -29,7 +32,7 @@ import kotlinx.android.synthetic.main.activity_login.email_sign_in_button
 import kotlinx.android.synthetic.main.activity_login.login_form
 import kotlinx.android.synthetic.main.activity_login.login_progress
 import kotlinx.android.synthetic.main.activity_login.password
-import java.util.ArrayList
+import java.util.*
 
 
 /**
@@ -37,239 +40,272 @@ import java.util.ArrayList
  */
 class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_login)
-		// Set up the login form.
-		populateAutoComplete()
-		password.setOnEditorActionListener(TextView.OnEditorActionListener { _, id, _ ->
-			if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
-				attemptLogin()
-				return@OnEditorActionListener true
-			}
-			false
-		})
 
-		email_sign_in_button.setOnClickListener { attemptLogin() }
-	}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        // Set up the login form.
+        populateAutoComplete()
+        configureButton()
+        password.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
 
-	private fun populateAutoComplete() {
-		if (!mayRequestContacts()) {
-			return
-		}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
 
-		loaderManager.initLoader(0, null, this)
-	}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                configureButton()
+            }
+        })
 
-	private fun mayRequestContacts(): Boolean {
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-			return true
-		}
-		if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-			return true
-		}
-		if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-			Snackbar.make(email, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-					.setAction(
-							android.R.string.ok
-					) {
-						requestPermissions(
-								arrayOf(READ_CONTACTS),
-								REQUEST_READ_CONTACTS
-						)
-					}
-		} else {
-			requestPermissions(arrayOf(READ_CONTACTS), REQUEST_READ_CONTACTS)
-		}
-		return false
-	}
+        email.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
 
-	/**
-	 * Callback received when a permissions request has been completed.
-	 */
-	override fun onRequestPermissionsResult(
-			requestCode: Int, permissions: Array<String>,
-			grantResults: IntArray
-	) {
-		if (requestCode == REQUEST_READ_CONTACTS) {
-			if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-				populateAutoComplete()
-			}
-		}
-	}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                configureButton()
+            }
+        })
+
+        password.setOnEditorActionListener(TextView.OnEditorActionListener { _, id, _ ->
+            if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+                attemptLogin()
+                return@OnEditorActionListener true
+            }
+            false
+        })
+        email_sign_in_button.setOnClickListener { attemptLogin() }
+    }
+
+    private fun configureButton() {
+        email_sign_in_button.isEnabled = password.length() >= 5 && email.length() >= 5
+    }
+
+    private fun populateAutoComplete() {
+        if (!mayRequestContacts()) {
+            return
+        }
+
+        loaderManager.initLoader(0, null, this)
+    }
+
+    private fun mayRequestContacts(): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true
+        }
+        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            return true
+        }
+        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
+            Snackbar.make(email, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(
+                            android.R.string.ok
+                    ) {
+                        requestPermissions(
+                                arrayOf(READ_CONTACTS),
+                                REQUEST_READ_CONTACTS
+                        )
+                    }
+        } else {
+            requestPermissions(arrayOf(READ_CONTACTS), REQUEST_READ_CONTACTS)
+        }
+        return false
+    }
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    override fun onRequestPermissionsResult(
+            requestCode: Int, permissions: Array<String>,
+            grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_READ_CONTACTS) {
+            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                populateAutoComplete()
+            }
+        }
+    }
 
 
-	/**
-	 * Attempts to sign in or register the account specified by the login form.
-	 * If there are form errors (invalid email, missing fields, etc.), the
-	 * errors are presented and no actual login attempt is made.
-	 */
-	private fun attemptLogin() {
-		// Reset errors.
-		email.error = null
-		password.error = null
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private fun attemptLogin() {
+        // Reset errors.
+        email.error = null
+        password.error = null
 
-		// Store values at the time of the login attempt.
-		val emailString = email.text.toString()
-		val passwordString = password.text.toString()
+        // Store values at the time of the login attempt.
+        val emailString = email.text.toString()
+        val passwordString = password.text.toString()
 
-		var cancel = false
-		var focusView: View? = null
+        var cancel = false
+        var focusView: View? = null
 
-		// Check for a valid password, if the user entered one.
-		if (!TextUtils.isEmpty(passwordString) && !isPasswordValid(passwordString)) {
-			password.error = getString(R.string.error_invalid_password)
-			focusView = password
-			cancel = true
-		}
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(passwordString) && !isPasswordValid(passwordString)) {
+            password.error = getString(R.string.error_invalid_password)
+            focusView = password
+            cancel = true
+        }
 
-		// Check for a valid email address.
-		if (TextUtils.isEmpty(emailString)) {
-			email.error = getString(R.string.error_field_required)
-			focusView = email
-			cancel = true
-		} else if (!isEmailValid(emailString)) {
-			email.error = getString(R.string.error_invalid_email)
-			focusView = email
-			cancel = true
-		}
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(emailString)) {
+            email.error = getString(R.string.error_field_required)
+            focusView = email
+            cancel = true
+        } else if (!isEmailValid(emailString)) {
+            email.error = getString(R.string.error_invalid_email)
+            focusView = email
+            cancel = true
+        }
 
-		if (cancel) {
-			// There was an error; don't attempt login and focus the first
-			// form field with an error.
-			focusView?.requestFocus()
-		} else {
-			// Show a progress spinner, and kick off a background task to
-			// perform the user login attempt.
-			showProgress(true)
-			createUser(User(emailString, passwordString, "Dummy", "User", 0))
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView?.requestFocus()
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true)
+            createUser(User(emailString, passwordString, "Dummy", "User", 0))
 //            mAuthTask = UserLoginTask(emailString, passwordString)
 //            mAuthTask!!.execute(null as Void?)
-		}
-	}
+        }
+    }
 
-	private fun isEmailValid(email: String): Boolean {
-		//TODO: Replace this with your own logic
-		return email.contains("@")
-	}
+    private fun isEmailValid(email: String): Boolean {
+        //TODO: Replace this with your own logic
+        return email.contains("@")
+    }
 
-	private fun isPasswordValid(password: String): Boolean {
-		//TODO: Replace this with your own logic
-		return password.length > 4
-	}
+    private fun isPasswordValid(password: String): Boolean {
+        //TODO: Replace this with your own logic
+        return password.length > 4
+    }
 
-	/**
-	 * Shows the progress UI and hides the login form.
-	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-	private fun showProgress(show: Boolean) {
-		val shortAnimTime = resources.getInteger(android.R.integer.config_shortAnimTime).toLong()
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private fun showProgress(show: Boolean) {
+        val shortAnimTime = resources.getInteger(android.R.integer.config_shortAnimTime).toLong()
 
-		login_form.visibility = if (show) View.GONE else View.VISIBLE
-		login_form.animate()
-				.setDuration(shortAnimTime)
-				.alpha((if (show) 0 else 1).toFloat())
-				.setListener(object : AnimatorListenerAdapter() {
-					override fun onAnimationEnd(animation: Animator) {
-						login_form.visibility = if (show) View.GONE else View.VISIBLE
-					}
-				})
+        login_form.visibility = if (show) View.GONE else View.VISIBLE
+        login_form.animate()
+                .setDuration(shortAnimTime)
+                .alpha((if (show) 0 else 1).toFloat())
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        login_form.visibility = if (show) View.GONE else View.VISIBLE
+                    }
+                })
 
-		login_progress.visibility = if (show) View.VISIBLE else View.GONE
-		login_progress.animate()
-				.setDuration(shortAnimTime)
-				.alpha((if (show) 1 else 0).toFloat())
-				.setListener(object : AnimatorListenerAdapter() {
-					override fun onAnimationEnd(animation: Animator) {
-						login_progress.visibility = if (show) View.VISIBLE else View.GONE
-					}
-				})
-	}
+        login_progress.visibility = if (show) View.VISIBLE else View.GONE
+        login_progress.animate()
+                .setDuration(shortAnimTime)
+                .alpha((if (show) 1 else 0).toFloat())
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        login_progress.visibility = if (show) View.VISIBLE else View.GONE
+                    }
+                })
+    }
 
-	override fun onCreateLoader(i: Int, bundle: Bundle?): Loader<Cursor> {
-		return CursorLoader(
-				this,
-				// Retrieve data rows for the device user's 'profile' contact.
-				Uri.withAppendedPath(
-						ContactsContract.Profile.CONTENT_URI,
-						ContactsContract.Contacts.Data.CONTENT_DIRECTORY
-				), ProfileQuery.PROJECTION,
+    override fun onCreateLoader(i: Int, bundle: Bundle?): Loader<Cursor> {
+        return CursorLoader(
+                this,
+                // Retrieve data rows for the device user's 'profile' contact.
+                Uri.withAppendedPath(
+                        ContactsContract.Profile.CONTENT_URI,
+                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY
+                ), ProfileQuery.PROJECTION,
 
-				// Select only email addresses.
-				ContactsContract.Contacts.Data.MIMETYPE + " = ?", arrayOf(
-				ContactsContract.CommonDataKinds.Email
-						.CONTENT_ITEM_TYPE
-		),
+                // Select only email addresses.
+                ContactsContract.Contacts.Data.MIMETYPE + " = ?", arrayOf(
+                ContactsContract.CommonDataKinds.Email
+                        .CONTENT_ITEM_TYPE
+        ),
 
-				// Show primary email addresses first. Note that there won't be
-				// a primary email address if the user hasn't specified one.
-				ContactsContract.Contacts.Data.IS_PRIMARY + " DESC"
-		)
-	}
+                // Show primary email addresses first. Note that there won't be
+                // a primary email address if the user hasn't specified one.
+                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC"
+        )
+    }
 
-	override fun onLoadFinished(cursorLoader: Loader<Cursor>, cursor: Cursor) {
-		val emails = ArrayList<String>()
-		cursor.moveToFirst()
-		while (!cursor.isAfterLast) {
-			emails.add(cursor.getString(ProfileQuery.ADDRESS))
-			cursor.moveToNext()
-		}
+    override fun onLoadFinished(cursorLoader: Loader<Cursor>, cursor: Cursor) {
+        val emails = ArrayList<String>()
+        cursor.moveToFirst()
+        while (!cursor.isAfterLast) {
+            emails.add(cursor.getString(ProfileQuery.ADDRESS))
+            cursor.moveToNext()
+        }
 
-		addEmailsToAutoComplete(emails)
-	}
+        addEmailsToAutoComplete(emails)
+    }
 
-	override fun onLoaderReset(cursorLoader: Loader<Cursor>) {
+    override fun onLoaderReset(cursorLoader: Loader<Cursor>) {
 
-	}
+    }
 
-	private fun addEmailsToAutoComplete(emailAddressCollection: List<String>) {
-		//Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-		val adapter = ArrayAdapter(
-				this@LoginActivity,
-				android.R.layout.simple_dropdown_item_1line, emailAddressCollection
-		)
+    private fun addEmailsToAutoComplete(emailAddressCollection: List<String>) {
+        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
+        val adapter = ArrayAdapter(
+                this@LoginActivity,
+                android.R.layout.simple_dropdown_item_1line, emailAddressCollection
+        )
 
-		email.setAdapter(adapter)
-	}
+        email.setAdapter(adapter)
+    }
 
-	object ProfileQuery {
-		val PROJECTION = arrayOf(
-				ContactsContract.CommonDataKinds.Email.ADDRESS,
-				ContactsContract.CommonDataKinds.Email.IS_PRIMARY
-		)
-		val ADDRESS = 0
-		val IS_PRIMARY = 1
-	}
+    object ProfileQuery {
+        val PROJECTION = arrayOf(
+                ContactsContract.CommonDataKinds.Email.ADDRESS,
+                ContactsContract.CommonDataKinds.Email.IS_PRIMARY
+        )
+        val ADDRESS = 0
+        val IS_PRIMARY = 1
+    }
 
-	@SuppressLint("StaticFieldLeak")
-	fun createUser(user: User) {
-		object : AsyncTask<Void, Void, Void>() {
-			override fun doInBackground(vararg voids: Void): Void? {
-				BeerRepository(this@LoginActivity).insertUser(user)
-				return null
-			}
+    @SuppressLint("StaticFieldLeak")
+    fun createUser(user: User) {
+        object : AsyncTask<Void, Void, Void>() {
+            override fun doInBackground(vararg voids: Void): Void? {
+                BeerRepository(this@LoginActivity).insertUser(user)
+                return null
+            }
 
-			override fun onPreExecute() {
-				super.onPreExecute()
-				showProgress(true)
-			}
+            override fun onPreExecute() {
+                super.onPreExecute()
+                showProgress(true)
+            }
 
-			override fun onPostExecute(result: Void?) {
-				super.onPostExecute(result)
-				showProgress(false)
-				finish()
-			}
+            override fun onPostExecute(result: Void?) {
+                super.onPostExecute(result)
+                showProgress(false)
 
-			override fun onCancelled() {
-				showProgress(false)
-			}
-		}.execute()
-	}
+                val intent = Intent(this@LoginActivity, ScrollingActivity::class.java)
+                startActivity(intent)
 
-	companion object {
-		/**
-		 * Id to identity READ_CONTACTS permission request.
-		 */
-		private val REQUEST_READ_CONTACTS = 0
-	}
+                finish() // link zur neuen Seite
+            }
+
+            override fun onCancelled() {
+                showProgress(false)
+            }
+        }.execute()
+    }
+
+    companion object {
+        /**
+         * Id to identity READ_CONTACTS permission request.
+         */
+        private val REQUEST_READ_CONTACTS = 0
+    }
 }
